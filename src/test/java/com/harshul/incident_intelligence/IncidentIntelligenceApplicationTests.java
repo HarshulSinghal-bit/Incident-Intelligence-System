ae68bc9c6f68ae10606877e745fe75e154d197e4
admin <harshulsinghal31@gmail.com>
2026-02-26 01:06:17 +0530

    CI Pipeline cleanup

--- src/test/java/com/harshul/incident_intelligence/IncidentIntelligenceApplicationTests.java
+++ src/test/java/com/harshul/incident_intelligence/IncidentIntelligenceApplicationTests.java
@@ -1,6 +1,5 @@
 package com.harshul.incident_intelligence;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
@@ -12,5 +11,4 @@ class IncidentIntelligenceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
