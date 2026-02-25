c5a65a2249c23d865a6d35cd1f3eae3c9ae83214
admin <harshulsinghal31@gmail.com>
2026-02-26 01:02:28 +0530

    Done by harshul

--- src/test/java/com/harshul/incident_intelligence/IncidentIntelligenceApplicationTests.java
+++ src/test/java/com/harshul/incident_intelligence/IncidentIntelligenceApplicationTests.java
@@ -2,6 +2,8 @@ package com.harshul.incident_intelligence;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 class IncidentIntelligenceApplicationTests {
